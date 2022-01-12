756b072b3de68d17b7c3b6788f816811b9e7fd19
CGAnVil <hoangminh.tlu@gmail.com>
2022-01-12 14:40:53 +0700

    upload bai tap

---
+++ src/com/codegym/Point2DTest.java
@@ -0,0 +1,11 @@
+package com.codegym;
+
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D point2d = new Point2D();
+        System.out.println(point2d);
+
+        point2d = new Point2D(3,7);
+        System.out.println(point2d);
+    }
+}
